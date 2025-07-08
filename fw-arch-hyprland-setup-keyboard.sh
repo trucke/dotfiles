aaac9958b61675e93c4e75e39bbe866055ed77f6
hiyagni <kt.code@pm.me>
2025-07-08 21:50:05 +0200

    update dotfiles

--- fw-arch-hyprland-setup-keyboard.sh
+++ fw-arch-hyprland-setup-keyboard.sh
@@ -85,5 +85,7 @@ fi
 
 configure_keyboard
 
-
-systemctl --user enable --now kanata.service
+systemctl --user enable kanata.service
+gum log --time TimeOnly --level info --time.foreground="#50fa7b" \
+    "Keyboard successfully configured. Changes will take effect after reboot or next login."
+gum confirm "Would you reboot the system?" && shutdown -r now
