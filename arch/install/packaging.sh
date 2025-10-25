8d9c87f71a35e4c9307befa1c91e20fcdefe2d08
hiyagni <kt.code@pm.me>
2025-10-25 20:10:42 +0200

    devcommit

--- arch/install/packaging.sh
+++ arch/install/packaging.sh
@@ -1,7 +1,9 @@
+sudo pacman -Syyy
+paru -Syy
 ################################################################################
 log "Install base packages..."
 mapfile -t packages < <(/usr/bin/grep -v '^#' "${DOTFILES_ARCH_INSTALL}/base.packages" | /usr/bin/grep -v '^$')
-sudo paru -S --noconfirm --needed "${packages[@]}"
+paru -S -q --noconfirm --needed "${packages[@]}"
 ################################################################################
 log "Install webapps..."
 omarchy-webapp-install "YouTube" https://youtube.com/ https://cdn.jsdelivr.net/gh/selfhst/icons/png/youtube.png
