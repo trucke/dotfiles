74add37d0f1d1f4fef777e169fbce428dd2b9a7e
hiyagni <kt.code@pm.me>
2025-10-25 20:31:36 +0200

    devcommit

--- arch/install/packaging.sh
+++ arch/install/packaging.sh
@@ -6,6 +6,7 @@ mapfile -t packages < <(/usr/bin/grep -v '^#' "${DOTFILES_ARCH_INSTALL}/base.pac
 paru -S -q --noconfirm --needed "${packages[@]}"
 ################################################################################
 log "Install webapps..."
+mkdir -p "${HOME}/.local/share/applications/icons"
 omarchy-webapp-install "YouTube" https://youtube.com/ https://cdn.jsdelivr.net/gh/selfhst/icons/png/youtube.png
 omarchy-webapp-install "GitHub" https://github.com/ https://cdn.jsdelivr.net/gh/selfhst/icons/png/github.png
 omarchy-webapp-install "T3Chat" https://t3.chat/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/t3-chat.png
