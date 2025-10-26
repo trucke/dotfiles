c3c5dcce916f8473d8b3e263a9379cfc3173c15c
hiyagni <kt.code@pm.me>
2025-10-26 22:49:00 +0100

    devcommit

--- arch/install/login.sh
+++ arch/install/login.sh
@@ -4,6 +4,25 @@ if [ "$(plymouth-set-default-theme)" != "heylogix" ]; then
     sudo plymouth-set-default-theme heylogix
 fi
 ################################################################################
+KEYRING_DIR="$HOME/.local/share/keyrings"
+KEYRING_FILE="$KEYRING_DIR/Default_keyring.keyring"
+DEFAULT_FILE="$KEYRING_DIR/default"
+mkdir -p $KEYRING_DIR
+cat <<EOF | tee "$KEYRING_FILE" >/dev/null
+[keyring]
+display-name=Default keyring
+ctime=$(date +%s)
+mtime=0
+lock-on-idle=false
+lock-after=false
+EOF
+cat <<EOF | tee "$DEFAULT_FILE"
+Default_keyring
+EOF
+chmod 700 "$KEYRING_DIR"
+chmod 600 "$KEYRING_FILE"
+chmod 644 "$DEFAULT_FILE"
+################################################################################
 log "Configure SDDM autologin..."
 sudo mkdir -p /etc/sddm.conf.d
 if [ ! -f /etc/sddm.conf.d/autologin.conf ]; then
