8895f2852d149d668421fb6787b8018aca3baa17
hiyagni <kt.code@pm.me>
2025-10-28 12:38:38 +0100

    devcommit

--- omarchy/install/packaging/base.sh
+++ omarchy/install/packaging/base.sh
@@ -1,3 +1,3 @@
 # Install all base packages
 mapfile -t packages < <(grep -v '^#' "$OMARCHY_INSTALL/base.packages" | grep -v '^$')
-sudo paru -Syu --noconfirm --needed "${packages[@]}"
+paru -Syu --noconfirm --needed "${packages[@]}"
