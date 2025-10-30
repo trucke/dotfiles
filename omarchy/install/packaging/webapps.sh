9f66cdfc548ce044d04229014343c6a7b95a72e7
hiyagni <kt.code@pm.me>
2025-10-30 22:34:10 +0100

    devcommit

--- omarchy/install/packaging/webapps.sh
+++ omarchy/install/packaging/webapps.sh
@@ -1,9 +1,7 @@
-omarchy-webapp-install "YouTube" \
-    https://youtube.com/ \
-    https://cdn.jsdelivr.net/gh/selfhst/icons/png/youtube.png
-omarchy-webapp-install "GitHub" \
-    https://github.com/ \
-    https://cdn.jsdelivr.net/gh/selfhst/icons/png/github.png
-omarchy-webapp-install "T3Chat" \
-    https://t3.chat/ \
-    https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/t3-chat.png
+omarchy-webapp-install "YouTube" https://youtube.com/ YouTube.png
+omarchy-webapp-install "GitHub" https://github.com/ GitHub.png
+omarchy-webapp-install "T3Chat" https://t3.chat/ T3Chat.png
+omarchy-webapp-install "Proton Mail" https://mail.proton.me/u/0/ Proton-Mail.png "" "x-scheme-handler/mailto"
+omarchy-webapp-install "Proton Calendar" https://calendar.proton.me/u/0/ Proton-Calendar.png
+omarchy-webapp-install "Proton Drive" https://drive.proton.me/u/0/ Proton-Drive.png
+omarchy-webapp-install "Lumo" https://lumo.proton.me/u/0/ Lumo.png
