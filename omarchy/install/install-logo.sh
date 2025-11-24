cab478425dc9d26771c3b95c96d256251fab7b67
hiyagni <kt.code@pm.me>
2025-11-24 21:31:41 +0100

    automated dev commit

--- omarchy/install/install-logo.sh
+++ omarchy/install/install-logo.sh
@@ -6,5 +6,6 @@ LOGO_PATH="$HOME/.dotfiles/omarchy/logo.png"
 sudo cp -f "${LOGO_PATH}" "${PLYMOUTH_THEME_PATH}"
 
 sudo plymouth reload
+sudo limine-update >/dev/null
 
 echo "Custom logo installed."
