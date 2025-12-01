f73856bcb987bd7981772f031a08ebe87a0052dd
hiyagni <kt.code@pm.me>
2025-12-01 16:14:46 +0100

    automated dev commit

--- omarchy/install/set-background.sh
+++ omarchy/install/set-background.sh
@@ -8,7 +8,7 @@ ln -nsf "$NEW_BACKGROUND" "$CURRENT_BACKGROUND_LINK"
 
 # Relaunch swaybg
 pkill -x swaybg
-setsid uwsm-app -- swaybg -i "$CURRENT_BACKGROUND_LINK" -m fill >/dev/null 2>&1 &
+setsid uwsm-app -- swaybg -i "$CURRENT_BACKGROUND_LINK" -m fill >/dev/null 2>&1
 
 echo ""
 echo "Background changed successfully."
