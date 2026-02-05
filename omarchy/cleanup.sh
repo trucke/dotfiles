2c556149e761f52b84d6f5bfc3a57640bb1ca6d1
hiyagni <kt.code@pm.me>
2026-02-05 19:22:15 +0100

    Remove Omarchy defaults that conflict with stowed dotfiles

--- omarchy/cleanup.sh
+++ omarchy/cleanup.sh
@@ -16,6 +16,12 @@ rm -f ~/.bash_history ~/.bash_logout ~/.bash_profile
 
 rm -rf ~/.config/{Typora,xournalpp,lazygit}
 
+# Remove Omarchy defaults that conflict with stowed dotfiles
+rm -f ~/.config/opencode/opencode.json
+rm -rf ~/.config/ghostty
+rm -rf ~/.config/git
+rm -f ~/.config/starship.toml
+
 ################################################################################
 
 echo "Cleanup finished."
