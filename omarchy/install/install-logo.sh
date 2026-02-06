9b5366fe25ea9c997fab70735d17e068a174bc76
hiyagni <kt.code@pm.me>
2026-02-06 17:28:03 +0100

    rebuild initramfs after replacing plymouth boot logo

--- omarchy/install/install-logo.sh
+++ omarchy/install/install-logo.sh
@@ -5,9 +5,14 @@ set -euo pipefail
 DOTFILES="${HOME}/.dotfiles/omarchy"
 PLYMOUTH_THEME_PATH="/usr/share/plymouth/themes/omarchy"
 
-# Plymouth boot logo
+# Replace plymouth boot logo and rebuild initramfs
 sudo cp -f "${DOTFILES}/logo.png" "${PLYMOUTH_THEME_PATH}/logo.png"
-sudo plymouth reload
-sudo limine-update >/dev/null
+sudo plymouth-set-default-theme omarchy
+
+if command -v limine-mkinitcpio &>/dev/null; then
+	sudo limine-mkinitcpio
+else
+	sudo mkinitcpio -P
+fi
 
 echo "Custom logo installed."
