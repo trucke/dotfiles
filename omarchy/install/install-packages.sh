1e9a3d434b84e2dca926d8fb84c28295e1a21677
hiyagni <kt.code@pm.me>
2026-02-06 17:22:03 +0100

    add progress messages during package cleanup

--- omarchy/install/install-packages.sh
+++ omarchy/install/install-packages.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 # Remove packages that conflict with ones we're about to install
+echo "Removing conflicting packages..."
 mapfile -t cleanup_packages <"$HOME/.dotfiles/omarchy/install/cleanup.packages"
 for package in "${cleanup_packages[@]}"; do
 	yay -Rnsu --noconfirm "$package" &>/dev/null
 done
+echo "Done."
 
 mapfile -t packages <"$HOME/.dotfiles/omarchy/install/install.packages"
 
